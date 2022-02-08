004e6acc6be5ae0e3f25733117bf18e64a1cd329
Yingmook <rawitsara.nu@gmail.com>
2022-02-08 23:34:05 +0700

    api dataset dashboard

--- main.py
+++ main.py
@@ -222,3 +222,202 @@ async def upload(vitek_id, background_tasks: BackgroundTasks, in_file: UploadFil
             "start_date": ""
         }
     }
+
+# ---------- DASHBOARD ----------
+
+@app.get("/api/lastest_version/")
+async def lastest_version(vitek_id):
+    query = sqlalchemy.text(
+        """ SELECT MAX(version)
+            FROM public.model_group
+            WHERE vitek_id = :vitek_id
+        """)
+    lastest_version = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id})
+
+    return {
+        "status": "success",
+        "data": {
+            "lastest_version": int(lastest_version.values[0][0])
+        }
+    }
+    
+@app.get("/api/antimicrobial_model/")
+async def antimicrobial_model(vitek_id):
+    query = sqlalchemy.text(
+        """ SELECT public.antimicrobial_answer.id, public.antimicrobial_answer.name
+            FROM public.model
+            INNER JOIN public.model_group_model ON model.id = model_group_model.model_id
+            INNER JOIN public.model_group ON public.model_group_model.model_group_id = public.model_group.id
+            INNER JOIN public.antimicrobial_answer ON public.model.antimicrobial_id = public.antimicrobial_answer.id
+            WHERE public.model_group.vitek_id = :vitek_id
+            GROUP BY public.antimicrobial_answer.id, public.antimicrobial_answer.name
+            ORDER BY public.antimicrobial_answer.name
+        """)
+    antimicrobial = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id})
+
+    return {
+        "status": "success",
+        "data": {
+            "antimicrobial": [{
+                'id': row[0],
+                'name': row[1]
+            } for row in antimicrobial.values]
+        }
+    }
+
+@app.get("/api/dashboard_case/")
+async def dashboard_case(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT to_char(public.report.report_issued_date, 'YYYY-MM'), COUNT(public.report.id)
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version
+            GROUP BY to_char(public.report.report_issued_date, 'YYYY-MM')
+            ORDER BY 1
+        """)
+    cases = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})
+    
+    return {
+        "status": "success",
+        "data": {
+            "cases": [{
+                'date': row[0],
+                'count': row[1]
+            } for row in cases.values]
+        }
+    }
+
+@app.get("/api/dashboard_species/")
+async def dashboard_species(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT public.species.name, COUNT(public.report.id)
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id 
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            INNER JOIN public.species ON public.report.species_id = public.species.id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version
+            GROUP BY public.species.name
+            ORDER BY COUNT(public.report.id) DESC
+        """)
+    species = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})
+    
+    return {
+        "status": "success",
+        "data": {
+            "species": [{
+                'name': row[0],
+                'count': row[1]
+            } for row in species.values]
+        }
+    }
+    
+@app.get("/api/dashboard_bacteria_genus/")
+async def dashboard_bacteria_genus(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT public.bacteria_genus.name, COUNT(public.report.id)
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id 
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            INNER JOIN public.bacteria_genus ON public.report.bacteria_genus_id = public.bacteria_genus.id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version
+            GROUP BY public.bacteria_genus.name
+            ORDER BY COUNT(public.report.id) DESC
+        """)
+    bacteria_genus = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})[:10]
+    
+    return {
+        "status": "success",
+        "data": {
+            "bacteria_genus": [{
+                'name': row[0],
+                'count': row[1]
+            } for row in bacteria_genus.values]
+        }
+    }
+    
+@app.get("/api/dashboard_submitted_sample/")
+async def dashboard_submitted_sample(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT public.submitted_sample.name, COUNT(public.report.id)
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id 
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            INNER JOIN public.submitted_sample ON public.report.submitted_sample_id = public.submitted_sample.id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version 
+            GROUP BY public.submitted_sample.name
+            ORDER BY COUNT(public.report.id) DESC
+        """)
+    submitted_sample = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})[:10]
+    
+    return {
+        "status": "success",
+        "data": {
+            "submitted_sample": [{
+                'name': row[0],
+                'count': row[1]
+            } for row in submitted_sample.values]
+        }
+    }
+    
+@app.get("/api/dashboard_antimicrobial_sir/")
+async def dashboard_antimicrobial_answer(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT public.antimicrobial_sir.name, 
+	            COUNT(CASE WHEN public.sir_sub_type.id=1 THEN 1 END) as "POS",
+	            COUNT(CASE WHEN public.sir_sub_type.id=2 THEN 1 END) as "NEG",
+ 	            COUNT(CASE WHEN public.sir_sub_type.id=3 THEN 1 END) as "S",
+	            COUNT(CASE WHEN public.sir_sub_type.id=4 THEN 1 END) as "I",
+	            COUNT(CASE WHEN public.sir_sub_type.id=5 THEN 1 END) as "R"
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            INNER JOIN public.report_sir ON public.report.id = public.report_sir.report_id
+            INNER JOIN public.antimicrobial_sir ON public.report_sir.antimicrobial_id = public.antimicrobial_sir.id
+            INNER JOIN public.sir_sub_type ON public.report_sir.sir_id = public.sir_sub_type.id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version
+            GROUP BY public.antimicrobial_sir.name
+            ORDER BY public.antimicrobial_sir.name
+        """)
+    antimicrobial_sir = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})
+    
+    return {
+        "status": "success",
+        "data": {
+            "antimicrobial_sir": [{
+                'name': row[0],
+                'count': {
+                    'pos': row[1],
+                    'neg': row[2],
+                    's': row[3],
+                    'i': row[4],
+                    'r': row[5]
+                }
+            } for row in antimicrobial_sir.values]
+        }
+    }
+    
+@app.get("/api/dashboard_antimicrobial_answer/")
+async def dashboard_antimicrobial_answer(vitek_id, version):
+    query = sqlalchemy.text(
+        """ SELECT public.antimicrobial_answer.name, COUNT(public.report.id)
+            FROM public.model_group
+            INNER JOIN public.model_group_file ON public.model_group.id = public.model_group_file.model_group_id
+            INNER JOIN public.report ON public.model_group_file.file_id = public.report.file_id
+            INNER JOIN public.report_answer ON public.report.id = public.report_answer.report_id
+            INNER JOIN public.antimicrobial_answer ON antimicrobial_answer.id = public.report_answer.antimicrobial_id
+            WHERE public.model_group.vitek_id = :vitek_id AND public.model_group.version = :version
+            GROUP BY public.antimicrobial_answer.name
+            ORDER BY COUNT(public.report.id) DESC
+        """)
+    antimicrobial_answer = pd.read_sql_query(query, conn, params={"vitek_id": vitek_id, "version": version})[:15]
+    
+    return {
+        "status": "success",
+        "data": {
+            "antimicrobial_answers": [{
+                'name': row[0],
+                'count': row[1]
+            } for row in antimicrobial_answer.values]
+        }
+    }
